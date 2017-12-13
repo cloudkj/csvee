305659bfddbf1458d67948586f7e12a864257366
cloudkj <cloudkj@users.noreply.github.com>
2017-12-12 21:11:34 -0800

    Use hash table to store columns to be selected

--- csvee.scm
+++ csvee.scm
@@ -2,24 +2,23 @@
 (use getopt-long)
 
 (define (parse line columns)
-  ;; TODO: optimizations - `columns` can be looked up in O(1) and collected as
-  ;; each index is examined
+  (define (select? col)
+    (hash-table-ref/default columns col #f))
   (define (output from to delim)
     (format #t (if delim ",~A" "~A") (substring line from to)))
-  (define (select from to count many?)
+  (define (select from to col selected)
     (cond ((>= to (string-length line))
            (begin
-             (when (member (+ count 1) columns)
-                   (output from to many?))
+             (when (select? col) (output from to (> selected 0)))
              (print)))
           ((eq? (string-ref line to) #\,)
            (begin
-             (when (member (+ count 1) columns)
-                   (output from to many?))
-             (select (+ to 1) (+ to 1) (+ count 1) #t)))
+             (when (select? col) (output from to (> selected 0)))
+             (select (+ to 1) (+ to 1) (+ col 1)
+                     (if (select? col) (+ selected 1) selected))))
           (else
-           (select from (+ to 1) count many?))))
-  (select 0 0 0 #f))
+           (select from (+ to 1) col selected))))
+  (select 0 0 1 0))
 
 (define (read-input columns)
   (let ((line (read-line)))
@@ -32,8 +31,10 @@
                           (single-char #\c)
                           (value #t))))
        (options (getopt-long (command-line-arguments) grammar))
-       (columns (map (compose string->number cdr)
-                     (filter (lambda (o) (equal? (car o) 'column)) options))))
-  (cond ((null? columns) (print (usage grammar)))
-        (else
-         (read-input columns))))
+       (columns (alist->hash-table
+                 (map (lambda (n) (cons n #t))
+                      (map (compose string->number cdr)
+                           (filter (lambda (o) (equal? (car o) 'column)) options))))))
+  (if (null? columns)
+      (print (usage grammar))
+      (read-input columns)))
